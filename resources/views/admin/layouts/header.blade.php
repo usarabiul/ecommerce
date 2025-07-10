69190c198110ceb681a542ca9b4b829137f0ead4
usarabiul <usarabiul@gmail.com>
2025-07-10 23:20:19 +0600

    Update header Short Link

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -6,37 +6,19 @@
 					<div class="top-menu-left d-none d-lg-block">
 				 	 <ul class="nav">
 						<li class="nav-item">
-						  <a class="nav-link" href="app-emailbox.html"><i class='bx bx-envelope'></i></a>
+						  <a class="nav-link" href="app-emailbox.html"><i class='bx bx-cart-alt'></i></a>
 						</li>
 						<li class="nav-item">
-						  <a class="nav-link" href="app-chat-box.html"><i class='bx bx-message'></i></a>
+						  <a class="nav-link" href="app-chat-box.html"><i class="bx bx-store-alt"></i> </a>
 						</li>
 						<li class="nav-item">
-						  <a class="nav-link" href="app-fullcalender.html"><i class='bx bx-calendar'></i></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="app-to-do.html"><i class='bx bx-check-square'></i></a>
+						  <a class="nav-link" href="{{route('index')}}"><i class='bx bx-globe'></i></a>
 						</li>
 					</ul>
 				   </div>
-					<div class="search-bar flex-grow-1">
-						<div class="position-relative search-bar-box">
-							<form>
-							  <input type="text" class="form-control search-control" autofocus placeholder="Type to search..."> <span class="position-absolute top-50 search-show translate-middle-y"><i class='bx bx-search'></i></span>
-							   <span class="position-absolute top-50 search-close translate-middle-y"><i class='bx bx-x'></i></span>
-						    </form>
-						</div>
-					</div>
 					<div class="top-menu ms-auto">
 						<ul class="navbar-nav align-items-center gap-1">
-							<li class="nav-item mobile-search-icon">
-								<a class="nav-link" href="javascript:;"><i class='bx bx-search'></i>
-								</a>
-							</li>
-							<li class="nav-item dark-mode d-none d-sm-flex">
-								<a class="nav-link dark-mode-icon" href="javascript:;"><i class='bx bx-moon'></i>
-								</a>
-							</li>
+							
 							<li class="nav-item dropdown dropdown-large">
 								<a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">	<i class='bx bx-category'></i>
 								</a>
@@ -348,8 +330,8 @@
 						<a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 							<img src="assets/images/avatars/avatar-2.png" class="user-img" alt="user avatar">
 							<div class="user-info ps-3">
-								<p class="user-name mb-0">Pauline Seitz</p>
-								<p class="designattion mb-0">Web Designer</p>
+								<p class="user-name mb-0">{{Str::limit(Auth::user()->name,15)}}</p>
+								<p class="designattion mb-0">Administrator</p>
 							</div>
 						</a>
 						<ul class="dropdown-menu dropdown-menu-end">
