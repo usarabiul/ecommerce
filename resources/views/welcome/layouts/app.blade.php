48a1960af14ccae9fdba9fb31e97bf0811b91479
usarabiul <usarabiul@gmail.com>
2025-09-29 23:02:45 +0600

    Front end New UI Setup and asset update

--- resources/views/welcome/layouts/app.blade.php
+++ resources/views/welcome/layouts/app.blade.php
@@ -13,39 +13,19 @@
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/x-icon" href="{{asset(general()->favicon())}}" />
         @yield('SEO')
-        <!-- Google Font CDN-->
-        <link href="https://fonts.googleapis.com/css?family=Source Code Pro" rel="stylesheet" />
         
-        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/demo.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/vendors/normalize.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/vendors/bootstrap.min.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/vendors/evara-font.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/vendors/uicons-regular-straight.css')}}" />
         
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/plugins/jquery-ui.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/plugins/magnific-popup.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/plugins/select2.min.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/plugins/slick.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/plugins/perfect-scrollbar.css')}}" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
-        
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,600,700" />
-
-        <!-- Bootstrap CS CDN -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-
-        <!-- Font Awesome CSS CDN-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-        
-        <!-- Slick Slider CSS CDN-->
-        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/slick.css')}}" />
-        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/slick-theme.css')}}" />
-        
-        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/fancybox.css')}}" />
-
-        <!-- Matis Menus CSS -->
-        <link rel="stylesheet" href="{{asset(assetLink().'/css/metisMenu.css')}}" />
-        
-        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/style.css')}}" />
+        <link rel="stylesheet" type="text/css" href="{{asset(assetLink().'/css/main.css')}}" />
         
         @stack('css')
     </head>
@@ -56,124 +36,128 @@
         @include(welcomeTheme().'layouts.header')
 
         <!--Main Content Section Start-->
-        <div class="main-content">
+        <main class="main">
         @yield('contents')
-        </div>
+        </main>
         <!--Main Content Section End-->
         
         <!--Footer Part Include Start-->
         @include(welcomeTheme().'layouts.footer')
-        
-        
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="{{asset(assetLink().'/js/hc-offcanvas-nav.js')}}"></script>
-        
-        <!-- Bootstrap Script  CDN-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <!--iconify Script CDN-->
-        <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-        <!-- Sweet Alert CDN -->
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
-        <!-- Metis Menus Script -->
-        <script src="{{asset(assetLink().'/js/metisMenu.min.js')}}"></script>
+        <!-- Vendor JS-->
+    <script src="{{asset(assetLink().'/js/vendor/modernizr-3.6.0.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/vendor/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/slick.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/jquery.syotimer.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/wow.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/jquery-ui.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/magnific-popup.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/select2.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/waypoints.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/counterup.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/images-loaded.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/isotope.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/scrollup.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/jquery.vticker-min.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/jquery.theia.sticky.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/plugins/jquery.elevatezoom.js')}}"></script>
+    <!-- Template  JS -->
+    <script src="{{asset(assetLink().'/js/main.js')}}"></script>
+    <script src="{{asset(assetLink().'/js/shop.js')}}"></script>
+     
 
-        <!-- Slick slider CDN -->
-        <script type="text/javascript" src="{{asset(assetLink().'/js/slick.min.js')}}"></script>
+      <script>
+          $(document).ready(function(){
 
-        <!-- Custom Script for this Design -->
-        <script src="{{asset(assetLink().'/js/myjquery.js')}}"></script>
+          $("#division").on("change", function(){
+              var id = $(this).val();
+                if(id==''){
+                  $('#district').empty().append('<option value="">No District</option>');
+                  $('#city').empty().append('<option value="">No City</option>');
+                }
+                var url ='{{url('geo/filter')}}' + '/'+id;
+                $.get(url,function(data){
+                  $('#district').empty().append(data.geoData);
+                  $('#city').empty().append('<option value="">No City</option>');
+                });   
+          });
 
-        <script>
-            $(document).ready(function(){
-
-            $("#division").on("change", function(){
-                var id = $(this).val();
-                  if(id==''){
-                   $('#district').empty().append('<option value="">No District</option>');
-                   $('#city').empty().append('<option value="">No City</option>');
-                  }
-                  var url ='{{url('geo/filter')}}' + '/'+id;
-                  $.get(url,function(data){
-                    $('#district').empty().append(data.geoData);
-                    $('#city').empty().append('<option value="">No City</option>');
-                  });   
-            });
-
-            $("#district").on("change", function(){
-                var id = $(this).val();
-                  if(id==''){
-                   $('#city').empty().append('<option value="">No City</option>');
-                  }
-                  var url ='{{url('geo/filter')}}' + '/'+id;
-                  $.get(url,function(data){
-                    $('#city').empty().append(data.geoData);  
-                  });   
-            });
-            
-        });
-        </script>
-        
-        <script>
-            $(document).on('click','.subsriberbtm',function(e){
-              e.preventDefault();
-               var url = $('#subscirbeForm').data('url');
-               var subscribeEmail =$('#subscribeEmail').val();
-                    $.ajax({
-                      url: url,
-                      type: 'POST',
-                      dataType: 'json',
-                       data: {email : subscribeEmail},
-                      cache: false,
-        
-                    })
-                    .done(function(data) {
-                        if(data.success)
-                          {
-                            $("#subscribeemailMsg").html("<p style='color: #f6f6f6;background: #009688;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>"+ data.message +"</p>");
-                            $("#subscribeEmail").css("border","");
-                            $("#subscirbeForm")[0].reset();
-                          }else{
-                            $("#subscribeemailMsg").html("<p style='color: #f6f6f6;background: #ff9800;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>"+ data.message +"</p>");
-                          }
-                    })
-                    .fail(function() {
-                      // alert("error");
-                    });
-        
-            });
-        
-            $("#subscribeEmail").keyup(function(){
-                  if(validateEmail()){
-                      $("#subscribeEmail").css("border","2px solid green");
-                      $("#subscribeemailMsg").html("<p style='color: #f6f6f6;background: #009688;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>Validated Email</p>");
-                  }else{
-                        var subscribeEmail=$("#subscribeEmail").val();
-                       if(subscribeEmail==''||subscribeEmail==null || subscribeEmail=='undefined'){
-                            $("#subscribeemailMsg").html("<p style='color: white;background: red;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>Please Get a Verified Email</p>");
+          $("#district").on("change", function(){
+              var id = $(this).val();
+                if(id==''){
+                  $('#city').empty().append('<option value="">No City</option>');
+                }
+                var url ='{{url('geo/filter')}}' + '/'+id;
+                $.get(url,function(data){
+                  $('#city').empty().append(data.geoData);  
+                });   
+          });
+          
+      });
+      </script>
+      
+      <script>
+          $(document).on('click','.subsriberbtm',function(e){
+            e.preventDefault();
+              var url = $('#subscirbeForm').data('url');
+              var subscribeEmail =$('#subscribeEmail').val();
+                  $.ajax({
+                    url: url,
+                    type: 'POST',
+                    dataType: 'json',
+                      data: {email : subscribeEmail},
+                    cache: false,
+      
+                  })
+                  .done(function(data) {
+                      if(data.success)
+                        {
+                          $("#subscribeemailMsg").html("<p style='color: #f6f6f6;background: #009688;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>"+ data.message +"</p>");
+                          $("#subscribeEmail").css("border","");
+                          $("#subscirbeForm")[0].reset();
                         }else{
-                          $("#subscribeEmail").css("border","2px solid red");
-                          $("#subscribeemailMsg").html("");
+                          $("#subscribeemailMsg").html("<p style='color: #f6f6f6;background: #ff9800;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>"+ data.message +"</p>");
                         }
-                  }
-              });
-        
-            function validateEmail(){
-                  var subscribeEmail=$("#subscribeEmail").val();
-        
-                   var reg =/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-                   if(reg.test(subscribeEmail)){
-                      return true;
-                   }else{
-                      return false;
-              }
-        
+                  })
+                  .fail(function() {
+                    // alert("error");
+                  });
+      
+          });
+      
+          $("#subscribeEmail").keyup(function(){
+                if(validateEmail()){
+                    $("#subscribeEmail").css("border","2px solid green");
+                    $("#subscribeemailMsg").html("<p style='color: #f6f6f6;background: #009688;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>Validated Email</p>");
+                }else{
+                      var subscribeEmail=$("#subscribeEmail").val();
+                      if(subscribeEmail==''||subscribeEmail==null || subscribeEmail=='undefined'){
+                          $("#subscribeemailMsg").html("<p style='color: white;background: red;margin: 5px 0;padding: 4px 5px;border-radius: 4px;font-weight: bold;line-height: 14px;font-size: 12px;'>Please Get a Verified Email</p>");
+                      }else{
+                        $("#subscribeEmail").css("border","2px solid red");
+                        $("#subscribeemailMsg").html("");
+                      }
+                }
+            });
+      
+          function validateEmail(){
+                var subscribeEmail=$("#subscribeEmail").val();
+      
+                  var reg =/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                  if(reg.test(subscribeEmail)){
+                    return true;
+                  }else{
+                    return false;
             }
-        </script>
-        
-        @stack('js')
+      
+          }
+      </script>
+      
+      @stack('js')
         
         
     </body>
